4a3c5e35661aa1ce388c27ab9d7555e03f15d0c6
TTKX1919 <142637922+TTKX1919@users.noreply.github.com>
2025-11-13 14:58:27 +0800

    Add files via upload

---
+++ notes/01-初中数学-黄冈中学/scripts/script.js
@@ -0,0 +1,8 @@
+function light() {
+  document.getElementById("mybody").style.backgroundColor = "white";
+  document.getElementById("mybody").style.color = "black";
+}
+function dark() {
+  document.getElementById("mybody").style.backgroundColor = "black";
+  document.getElementById("mybody").style.color = "white";
+}
