42a0c5a2619b385b0d482cbc4ec5387d2bacbabb
TTKX1919 <142637922+TTKX1919@users.noreply.github.com>
2025-11-20 11:24:03 +0800

    Add files via upload

--- scripts/mathdreaft.js
+++ scripts/mathdreaft.js
@@ -1,67 +1,14 @@
-function vtext() {
+window.onload = function autoviewtext() {
   var textArea = document.getElementById("text1");
+  textArea.value =
+    "示例：2<math><msqrt>x</msqrt></math>=y x=100 &#8756;y=2<math><msqrt>100</msqrt></math>=20，点击键盘上的空格显示：";
   textArea.addEventListener("input", (event) => {
     var output = document.getElementById("viewarea");
     output.innerHTML = event.target.value;
   });
   textArea.selectionEnd = textArea.value.length;
   textArea.focus();
-}
-function madd() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#43;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mreduce() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#45;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mride() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#42;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mdivide() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#47;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mequal() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#61;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
+};
 function msup() {
   var textArea = document.getElementById("text1");
   textArea.value = textArea.value + "<sup></sup>";
@@ -82,94 +29,6 @@ function msub() {
   });
   const length = textArea.value.length;
   textArea.selectionEnd = length - 6;
-  textArea.focus();
-}
-function mlparenthesis() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#40;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mrparenthesis() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#41;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mlbracket() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#91;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mrbracket() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#93;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mlbrace() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#123;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mrbrace() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#125;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mlabracket() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#60;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
-  textArea.focus();
-}
-function mrabracket() {
-  var textArea = document.getElementById("text1");
-  textArea.value = textArea.value + "&#62;";
-  textArea.addEventListener("input", (event) => {
-    var output = document.getElementById("viewarea");
-    output.innerHTML = event.target.value;
-  });
-  const length = textArea.value.length;
-  textArea.selectionEnd = length;
   textArea.focus();
 }
 function msqrt() {
@@ -198,6 +57,248 @@ function mroot() {
 function mareduce() {
   var textArea = document.getElementById("text1");
   textArea.value = textArea.value + "&#177;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mcone() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#9312;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mctwo() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#9313;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mcthree() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#9314;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mcfour() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#9315;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mcfive() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#9316;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mcesign() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#128;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mpmille() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#137;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mbullet() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#149;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mtrademark() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#153;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mpound() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#163;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function myen() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#165;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mcopyright() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#169;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mrtrademark() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#174;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function minfinity() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#8734;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mangle() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#8736;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mbecause() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#8757;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mtherefore() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#8756;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function maequal() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#8776;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mnequal() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#8800;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mloequal() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#8804;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mgoequal() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#8805;";
+  textArea.addEventListener("input", (event) => {
+    var output = document.getElementById("viewarea");
+    output.innerHTML = event.target.value;
+  });
+  const length = textArea.value.length;
+  textArea.selectionEnd = length;
+  textArea.focus();
+}
+function mperpendicular() {
+  var textArea = document.getElementById("text1");
+  textArea.value = textArea.value + "&#8869;";
   textArea.addEventListener("input", (event) => {
     var output = document.getElementById("viewarea");
     output.innerHTML = event.target.value;
